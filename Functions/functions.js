92da33ee489354215dcf390d70d5cb04eae9a6df
Cristian Malaga <cmalaga0605@gmail.com>
2022-03-15 20:58:12 -0400

    add items

---
+++ Functions/functions.js
@@ -0,0 +1,13 @@
+// sum function
+function sumExample1 (a, b){
+   return a + b;
+}
+// arrow function sum function
+const sumExample2 = (a, b) => {
+    return a + b;
+}
+
+// anonymous function
+(a, b) => {
+    return a + b;
+}
---
+++ Variables/variables.js
@@ -0,0 +1,25 @@
+// legacy
+var name = "John";
+// new
+let name = "John";
+
+// const variable is new and does not change
+const lastName = "Yenny";
+
+// data types
+// string
+let stringExample = "";
+// number
+let numberExample = 1234;
+// symbol
+let symbolExample = Symbol("example");
+// object
+let objectExample = {
+    name: "john",
+    lastName: "ummi",
+    age: 40
+}
+// dates
+let dateExample = new Date();
+// arrays
+let arrayExample = [1,2,3,5];
